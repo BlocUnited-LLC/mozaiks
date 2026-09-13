1708ae83f07dc2490c27317612a6ea8517e6b372
mbari <info@blocunited.com>
2026-09-13 17:11:28 -0400

    fix(ask): close generated page context contracts
    Signed-off-by: mbari <info@blocunited.com>

--- factory_app/workflows/AppGenerator/tools/app_validation.py
+++ factory_app/workflows/AppGenerator/tools/app_validation.py
@@ -2254,7 +2254,9 @@ async def run_app_bundle_acceptance_gate(
     }
 
     agent_integration_result = await _agent_backend_integration_result(context_variables)
-    wiring_result = await validate_wiring(context_variables=context_variables)
+    wiring_result = await validate_wiring(context_variables={"generated_files": generated_files})
+    _context_set(context_variables, "wiring_validation_passed", wiring_result["passed"])
+    _context_set(context_variables, "wiring_validation_result", wiring_result)
     module_implementation_result = validate_module_implementation_contract(generated_files)
     runtime_quality_result = _runtime_quality_result(generated_files)
     functional_diagnostics = scan_functional_generated_app(
